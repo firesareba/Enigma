06cec123f659c4f8f37df4ba5b8ed5c98c1b5e8e
firesareba <101755187+firesareba@users.noreply.github.com>
2025-12-23 21:30:43 -0800

    Lampboard animation
    Thinking of adding a fade animation. But will first work on keyboard and then plugboard and maybe the rotors as well

--- script.js
+++ script.js
@@ -34,9 +34,15 @@ rotors_to_use.addEventListener(
 );
 
 plaintext.addEventListener(
-    "change", function(event) {
+    "change", async function(event) {
         for (const char of plaintext.value.toUpperCase()){
-            encrypt(char);
+            encrypted_char = encrypt(char);
+            ciphertext.innerHTML += encrypted_char;
+            document.getElementById(encrypted_char).style.backgroundColor = "yellow";
+            document.getElementById(encrypted_char).style.color = "black";
+            await sleep(1000);
+            document.getElementById(encrypted_char).style.backgroundColor = "black";
+            document.getElementById(encrypted_char).style.color = "white";
         }
         plaintext.value = "";
   }
@@ -118,5 +124,10 @@ function encrypt(char){
         //plugboard
         char = apply_plugboard(char);
     }
-    ciphertext.innerHTML += char;
+    return char;
 }
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
